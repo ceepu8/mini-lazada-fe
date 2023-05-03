c761b8f651b6fcaaa3942e001740510791e28848
Uyen Cao <caophuonguyen132002@gmail.com>
2023-05-03 16:02:03 +0700

    build add product form

--- controller/vendor.js
+++ controller/vendor.js
@@ -2,7 +2,7 @@ const API_URL = "https://smoky-mini-lazada-be.onrender.com/api";
 
 const USER = {
   accessToken:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJJZCI6IjY0NGY2YmY5OWEzZTI1YTE2YmIwODcwOSIsInVzZXJuYW1lIjoidXllbmNhb2J1c2luZXNzIiwicm9sZSI6InZlbmRvciJ9LCJpYXQiOjE2ODMwMjkwNzQsImV4cCI6MTY4MzA2NTA3NH0.ppuCwmQRDJYVxdBuGFv0pcs16ZyngmLZT_KFd8wE9q4",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJJZCI6IjY0NGY2YmY5OWEzZTI1YTE2YmIwODcwOSIsInVzZXJuYW1lIjoidXllbmNhb2J1c2luZXNzIiwicm9sZSI6InZlbmRvciJ9LCJpYXQiOjE2ODMxMDQ0MTQsImV4cCI6MTY4MzE0MDQxNH0.rX-KPJ1T6LeDM7zAO2ED0iBCLNkXhQSrQRMUaPLL284",
   user: {
     username: "uyencaobusiness",
     role: "vendor",
@@ -42,6 +42,54 @@ const renderProduct = async () => {
     productTable.innerHTML = html;
   } catch (error) {
     console.log(error);
+  }
+};
+
+window.addProduct = async () => {
+  const formData = new FormData();
+
+  const name = document.getElementById("form-control-input-name").value;
+  const price = document.getElementById("form-control-input-price").value;
+  const description = document.getElementById(
+    "form-control-textarea-description"
+  ).value;
+  const file = document.getElementById("form-control-file-image").files[0];
+
+  formData.append("file", file);
+  formData.append("name", name);
+  formData.append("price", price);
+  formData.append("description", description);
+
+  try {
+    const { data } = await axios({
+      method: "post",
+      url: `${API_URL}/product`,
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${USER.accessToken}`,
+      },
+    });
+
+    console.log(data);
+    renderProduct();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.readURL = (input) => {
+  if (input.files && input.files[0]) {
+    console.log(input.files);
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      document
+        .getElementById("product-preview-image")
+        .setAttribute("src", e.target.result);
+    };
+
+    reader.readAsDataURL(input.files[0]);
   }
 };
 
