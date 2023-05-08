64a1986ad9bce0ebeff072ab0e11446e78c4b29a
ceepu8 <caophuonguyen132002@gmail.com>
2023-05-08 21:40:05 +0700

    update getting form value

--- controller/login.js
+++ controller/login.js
@@ -1,12 +1,18 @@
-function validation(){
-	var username = document.querySelector("#username");
-	var password = document.querySelector("#password");
-	var error_msg = document.querySelector(".error_msg");
+function handleForm(e) {
+  e.preventDefault();
+  var username = document.querySelector("#username");
+  var password = document.querySelector("#password");
+  var error_msg = document.querySelector(".error_msg");
 
-	if(username.value.trim() == '' || password.value.trim() == '' ){
-		error_msg.style.display = "inline-block";
-		username.style.border = "1px solid #f74040";
-		password.style.border = "1px solid #f74040";
-		return false;
-	}
+  if (username.value.trim() == "" || password.value.trim() == "") {
+    error_msg.style.display = "inline-block";
+    username.style.border = "1px solid #f74040";
+    password.style.border = "1px solid #f74040";
+    return false;
+  }
 }
+
+window.onload = () => {
+  var form = document.getElementById("login-form");
+  form.addEventListener("submit", handleForm);
+};
