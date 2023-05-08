c9ec050db6a8a716e36145edead2c1ba3574f693
khang <s3963613@rmit.edu.vn>
2023-05-08 11:57:45 +0700

    update

---
+++ controller/login.js
@@ -0,0 +1,12 @@
+function validation(){
+	var username = document.querySelector("#username");
+	var password = document.querySelector("#password");
+	var error_msg = document.querySelector(".error_msg");
+
+	if(username.value.trim() == '' || password.value.trim() == '' ){
+		error_msg.style.display = "inline-block";
+		username.style.border = "1px solid #f74040";
+		password.style.border = "1px solid #f74040";
+		return false;
+	}
+}
