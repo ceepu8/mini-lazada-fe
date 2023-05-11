dbd0a6fb4989cbda73bd8c2a3400b1f891962802
khang <s3963613@rmit.edu.vn>
2023-05-11 17:37:25 +0700

    update feature

--- controller/profile.js
+++ controller/profile.js
@@ -1,4 +1,4 @@
 var loadFile = function (event) {
-    var image = document.getElementById("output");
+    var image = document.getElementById("avatar");
     image.src = URL.createObjectURL(event.target.files[0]);
     };
