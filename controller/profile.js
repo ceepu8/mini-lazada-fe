8e2d9dea247df15ef547b4ad46ce98a6b6ae8b5c
khang <s3963613@rmit.edu.vn>
2023-05-11 18:02:57 +0700

    feature profile page

--- controller/profile.js
+++ controller/profile.js
@@ -1,3 +1,30 @@
+
+function loadUser() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://smoky-mini-lazada-be.onrender.com/api/user");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.setRequestHeader("Authorization", "Bearer "+jwt);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+        const objects = JSON.parse(this.responseText);
+        if (objects["status"] == "ok") {
+            const user = objects["user"]
+            document.getElementById("name").innerHTML = user["name"];
+            document.getElementById("avatar").src = user["avatar"];
+            document.getElementById("username").innerHTML = user["username"];
+            document.getElementById("address").innerHTML = user["address"];
+            document.getElementById("email").innerHTML = user["email"];
+            document.getElementById("businessName").innerHTML = user["businessName"];
+            document.getElementById("businessAddress").innerHTML = user["businessAddress"];
+        }
+    }
+};
+}
+      
+loadUser();
+      
+    
 var loadFile = function (event) {
     var image = document.getElementById("avatar");
     image.src = URL.createObjectURL(event.target.files[0]);
