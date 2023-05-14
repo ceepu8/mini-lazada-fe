ce1344926815bc9c9463daef117c71712b8baf31
ceepu8 <caophuonguyen132002@gmail.com>
2023-05-14 16:22:45 +0700

    Merge branch 'main' of github.com:ceepu8/mini-lazada-fe into HEAD

--- controller/login.js
+++ controller/login.js
@@ -1,20 +1,59 @@
-import { handleFormValidation } from "./validation";
+import { handleFormValidation } from "./validation.js";
+import { loginApi } from "../api/loginApi.js"
 
-function handleForm(e) {
+const spinner = `
+    <span
+    class="spinner-border spinner-border-sm"
+    role="status"
+    aria-hidden="true"
+    ></span>
+    Loading...
+
+`
+const handleForm = async (e) => {
   e.preventDefault();
-  var username = document.querySelector("#username");
-  var password = document.querySelector("#password");
-
   const isError = handleFormValidation("#login-form");
+  if (isError) return
 
-  console.log(isError);
+  const loginBtn = document.querySelector(".btn.submit-btn")
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-  //   if (username.value.trim() == "" || password.value.trim() == "") {
-  //     error_msg.style.display = "inline-block";
-  //     username.style.border = "1px solid #f74040";
-  //     password.style.border = "1px solid #f74040";
-  //     return false;
-  //   }
+  const loginData = { username, password }
+
+  try {
+    // SET SPINNER FOR SUBMIT BUTTON
+    loginBtn.innerHTML = spinner
+    loginBtn.setAttribute("disabled", true)
+
+    const { data, status } = await loginApi.login(loginData);
+    if (status === 200) {
+      const user = data.user
+      localStorage.setItem("user", JSON.stringify(
+        {
+          accessToken: data.accessToken,
+          user: data.user
+        }
+      ))
+      const userRole = user.role
+      swal({
+        title: "Login successfully!",
+        icon: "success",
+        button: "Move to next page!",
+      }).then(() => {
+        window.location.assign(`../../pages/${userRole}/${userRole}.html`);
+      });
+    }
+  } catch (error) {
+    // RESET FOR SUBMIT BUTTON
+    swal({
+      title: "ERROR!",
+      text: error.response?.data?.message,
+    })
+  }
+
+  loginBtn.innerHTML = "Login"
+  loginBtn.removeAttribute("disabled")
 }
 
 window.onload = () => {
